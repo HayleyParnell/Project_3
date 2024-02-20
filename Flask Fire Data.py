aba1472d6e622ec8cd4aef8fa30536b9b07b58aa
ArtienVoskanian <arvoskani@gmail.com>
2024-02-19 18:39:12 -0800

    Updated Flask file, index file, added Templates and static folders and moved files into them

--- Flask Fire Data.py
+++ Flask Fire Data.py
@@ -1,10 +1,17 @@
 from flask import Flask, jsonify
+from flask import Flask, render_template
 from flask_cors import CORS
 import sqlite3
 
 
 app = Flask(__name__)
 CORS(app)
+
+@app.route("/")
+def home_page():
+    return render_template('index.html')
+    
+
 
 @app.route('/data')
 def get_data():
@@ -19,6 +26,14 @@ def get_data():
     conn.close()
 
     return jsonify(data)
+
+@app.route('/US')
+def us_visualization():
+    return render_template('Fire Visualization.html')
+
+# @app.route('/3D')
+# def globe_visualization():
+#     return render_template('')
     
 if __name__ == '__main__':
     app.run(debug=True)
