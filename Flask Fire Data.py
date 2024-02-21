00e8efad084abae58cde1a57b74991b9c6a8ce44
Artien Voskanian <147466747+ArtienVoskanian@users.noreply.github.com>
2024-02-20 20:59:54 -0800

    Artien (#5)
    * Flask python file
    
    * Jupyter notebook that transforms the csv's into sqlite databses
    
    * Updated Jupyter notebook, commented out unnecessary steps
    
    * Starter visualization code
    
    * Starter HTML file for visualization
    
    * Updated the html code
    
    * Updated Jup. Notebook to take sample of data, Updated JS code for visualization
    
    * style sheet
    
    * Added legend to Visualization. Fixed minor bug in JS code
    
    * Updated Flask file, index file, added Templates and static folders and moved files into them
    
    * Finalized Flask, html, style sheets, and JS code
    
    * Final commit
    
    * Delete fire_db
    
    * Delete Fire Data Visualization.ipynb

---
+++ Flask Fire Data.py
@@ -0,0 +1,53 @@
+from flask import Flask, jsonify
+from flask import Flask, render_template
+from flask_cors import CORS
+from flask import Flask, send_file
+from flask import Flask, send_from_directory
+import sqlite3
+
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def home_page():
+    return render_template('index.html')
+    
+
+
+@app.route('/data')
+def get_data():
+    # Connect to SQLite database
+    conn = sqlite3.connect('fire_db')
+    cursor = conn.cursor()
+    
+    # Execute SQL query
+    cursor.execute('SELECT * FROM fire_data')
+    data = cursor.fetchall()
+    
+    conn.close()
+
+    return jsonify(data)
+
+@app.route('/US')
+def us_visualization():
+    return render_template('Fire Visualization.html')
+
+@app.route('/3D')
+def globe_visualization():
+    return render_template('map.html')
+
+@app.route('/frp')
+def get_image():
+    return send_file('static/fire_incidents_3d_heatmap.png', mimetype='image/png')
+
+@app.route('/heatmap')
+def heatmap():
+    return send_file('Templates/Heatmap.html')
+
+@app.route('/static-heatmap/<path:filename>')
+def static_heatmap_files(filename):
+    return send_from_directory('templates/static-heatmap', filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
